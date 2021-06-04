907fb4855e26e61add16a2a2066e8b432754acf4
taisei0123 <taisei8955@gmail.com>
2021-06-05 01:51:09 +0900

    create card of menu page

---
+++ react_native/src/components/Menu/PullUp.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {StyleSheet, View, Image} from 'react-native';
+import {Button, Card} from '@ui-kitten/components';
+
+const Footer = () => (
+  <View style={[styles.footerContainer]}>
+    <Button style={styles.footerControl} size="small">
+      PLAY!
+    </Button>
+  </View>
+);
+
+const PullUp = () => (
+  <>
+    <Card style={styles.card}>
+      <Image
+        source={require('../Images/PullUp.jpg')}
+        style={{width: 350, height: 200}}
+      />
+      <Footer />
+    </Card>
+  </>
+);
+
+const styles = StyleSheet.create({
+  card: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: 2,
+  },
+  footerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  footerControl: {
+    margin: 10,
+  },
+});
+
+export default PullUp;
--- react_native/src/components/screens/Menu.tsx
+++ react_native/src/components/screens/Menu.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import {Layout, Text, Button} from '@ui-kitten/components';
+import {Layout, Button} from '@ui-kitten/components';
 import {StyleSheet} from 'react-native';
+import Squat from '../Menu/Squat';
+import PushUp from '../Menu/PushUp';
+import PullUp from '../Menu/PullUp';
+import ArmCurl from '../Menu/ArmCurl';
+import ShoulderRaise from '../Menu/ShoulderRaise';
 
 const Menu = ({navigation}) => {
   return (
     <Layout style={styles.container}>
-      <Text>Menu</Text>
+      <Squat />
+      <PushUp />
+      <PullUp />
+      <ArmCurl />
+      <ShoulderRaise />
       <Button onPress={() => navigation.navigate('Profile')}>
         Go to Profile
       </Button>
